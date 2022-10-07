d69d7311d89f1b1eb7d3a3b60f0be5741dcceb6a
Rauf Erk <erk.rauf@gmail.com>
2022-10-07 14:10:56 +0300

    romanDigits

--- src/romanToInt.ts
+++ src/romanToInt.ts
@@ -1,49 +1,22 @@
+const roman = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  M: 1000,
+}
+
 function romanToInt(s: string): number {
-  // const M = 1000
-  // const C = 100
-  // const X = 10
-  // const L = 50
-  // const V = 5
-  // const I = 1
-
-  return s.split('').reduce((acc, el) => {
-    switch (el) {
-      case 'I':
-        return acc + 1
-        break
-
-      case 'V':
-        return acc + 5
-        break
-
-      case 'L':
-        return acc + 50
-        break
-
-      case 'X':
-        return acc + 10
-        break
-
-      case 'C':
-        return acc + 100
-        break
-
-      case 'D':
-        return acc + 500
-        break
-
-      case 'M':
-        return acc + 1000
-        break
-
-      default:
-        return acc
-        break
-    }
+  return s.split('').reduce((acc, el, index, array) => {
+    const slice = array.slice(index + 1)
+    const koef = slice.some((arrEl) => roman[arrEl] > roman[el]) ? -1 : 1
+    return acc + koef * roman[el]
   }, 0)
 }
 
 console.log(romanToInt('CM'))
+console.log(romanToInt('MC'))
 console.log(romanToInt('III'))
 console.log(romanToInt('LVIII'))
 console.log(romanToInt('MCMXCIV'))
