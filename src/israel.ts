3f143fc6f9cf50f5478e86aa76c4340f7a57bfc2
Rauf Erk <erk.rauf@gmail.com>
2022-10-10 19:40:55 +0300

    addSeolvedTastFromHebrew

--- src/israel.ts
+++ src/israel.ts
@@ -24,104 +24,32 @@
 // // 3 b
 // // ========================
 
-// //const callBackFoo = (res)=>{ console.log("1 ", res); return "a"; }
-
-// class Deferred {
-//   static arr: string[] = []
-//   constructor() {
-//     this.then = (foo) => {
-//       const res = foo()
-//       Deferred.arr.push(res)
-//     }
-//     this.resolve = (param) => {
-//       Deferred.arr.forEach((item, index) => {
-//         if (index === 0) {
-//           console.log(index + 1, param)
-//         } else {
-//           console.log(index + 1, item)
-//         }
-//       })
-//     }
-//   }
-// }
-// const res = 'res'
-
-// class Different extends Promise
-
-// class Different extends Promise {
-//   constructor(name: any) {
-//     super((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(1)
-//       }, 1)
-//     })
-//     this.name = name
-//   }
-// }
-
-// const d = new Different()
-// d.then((data) => console.log())
-
-// const myPomise = new Promise((res, rej) => setTimeout(() => res('YES'), 1000))
-// myPomise.then((data) => console.log(data))
-
-class Deferred extends Promise<string> {
-  index = 0
-  constructor() {
-    super((resolve, reject) => {
+class Deferred {
+  static buffer = ''
+  then(param: (txt: string) => string) {
+    return new Promise((res, rej) => {
       setTimeout(() => {
-        // console.log(this.myFoo.next(), '<<=')
-        resolve(++this.index + '')
-        // reject()
-      }, 1000)
+        Deferred.buffer = param(Deferred.buffer)
+      }, 0)
     })
   }
   resolve(param: string) {
-    console.log(++this.index + '', param)
-  }
-
-  static get [Symbol.species]() {
-    return Promise
+    Deferred.buffer = param
   }
 }
 
-// const p = new MyPromise('HeeeYY!')
-// p.then((data) => console.log(data))
-// p.resolve('hellow!')
-
-var d = new Deferred()
-d.then((res) => {
+const d = new Deferred()
+d.then(function (res) {
   console.log('1 ', res)
   return 'a'
-}).catch((err) => console.log(err))
-
+})
 d.then(function (res) {
   console.log('2 ', res)
   return 'b'
-}).catch((err) => console.log(err))
-
+})
 d.then(function (res) {
   console.log('3 ', res)
   return 'c'
-}).catch((err) => console.log(err))
-// // at this point, nothing is printed to console (yet)
+})
 
 d.resolve('hello')
-// // output of usage example
-// // 1 hello
-// // 2 a
-// // 3 b
-
-// const foo = function* (param: string) {
-//   let index = 0
-
-//   while (true) {
-//     yield index++
-//   }
-// }
-
-// const myGen = foo('test')
-
-// console.log(myGen.next(111))
-// console.log(myGen.next(222))
-// console.log(myGen.next(333))
