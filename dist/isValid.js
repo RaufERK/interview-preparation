7a477cdd3e11d9e686f1987ecf9c82d91dfaf58e
Rauf Erk <erk.rauf@gmail.com>
2022-10-12 18:15:59 +0300

    isValid

---
+++ dist/isValid.js
@@ -0,0 +1,27 @@
+"use strict";
+console.log('===== isValid =====>');
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('(]'));
+function isValid(s) {
+    const alphabet = {
+        '{': '}',
+        '[': ']',
+        '(': ')',
+    };
+    const stack = [];
+    const array = s.split('');
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const last = stack[stack.length - 1];
+        console.log(element, last);
+        if (element === last) {
+            stack.pop();
+        }
+        else {
+            stack.push(alphabet[element]);
+        }
+    }
+    // console.log('FINALL==>', stack)
+    return stack.length === 0 ? true : false;
+}
---
+++ src/isValid.ts
@@ -0,0 +1,30 @@
+console.log('===== isValid =====>')
+
+console.log(isValid('()'))
+console.log(isValid('()[]{}'))
+console.log(isValid('(]'))
+
+function isValid(s: string): boolean {
+  const alphabet = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+  }
+
+  const stack: string[] = []
+
+  const array = s.split('')
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index]
+    const last = stack[stack.length - 1]
+    console.log(element, last)
+    if (element === last) {
+      stack.pop()
+    } else {
+      stack.push(alphabet[element])
+    }
+  }
+  // console.log('FINALL==>', stack)
+
+  return stack.length === 0 ? true : false
+}
